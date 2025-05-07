4fc04a6d0b93be449aadf0a6c043eb567aefce42
mtrekels <maarten.trekels@plantentuinmeise.be>
2025-05-07 23:23:57 +0200

    fixing GRIIS module

---
+++ b3alien/griis/__init__.py
@@ -0,0 +1,11 @@
+'''
+
+Documentation needs to be added here
+
+'''
+
+from .griis import CheckList
+from .griis import get_speciesKey
+from .griis import do_taxon_matching
+from .griis import read_checklist
+from .griis import split_event_date
